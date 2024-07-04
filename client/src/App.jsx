8f90c4b018497d96c09097a7bf4642264a34ac17
tmarr1426 <tmarr1426@gmail.com>
2024-07-04 16:35:27 -0500

    Working login/signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 
-import { Auth, Login, Singup, Dashboard, PuttingSession, Nav } from "./index";
+import { Auth, Dashboard, PuttingSession, Nav } from "./index";
 
 function App() {
   const [sessionToken, setSessionToken] = useState(false);
@@ -16,13 +16,15 @@ function App() {
   }, []);
 
   // Sets the Token for the user session in the localstorage of the website.
-  const updateToken = (token, userType) => {
+  const updateToken = (token) => {
     console.log("Token Updated", token);
-
-    localStorage.setItem("MyToken", token);
-
-    setSessionToken(token);
-  };
+    if (token) {
+        localStorage.setItem("MyToken", token);
+        setSessionToken(token);
+    } else {
+        console.error("Token is undefined");
+    }
+};;
 
   // Clears the Token in the local storage so a new user can sign on.
   const clearToken = () => {
@@ -48,6 +50,7 @@ function App() {
                 element={<Auth updateToken={updateToken} />}
               />
             </Routes>
+            <Auth updateToken={updateToken}/>
           </div>
         </>
       )}
@@ -59,7 +62,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/stats" element={<Stats />} />
+            <Route path="/putting-session" element={<PuttingSession />} />
           </Routes>
         </>
       )}
